3d166a60e2555113939f86625a73482af0103563
Dolph Mathews <dolph.mathews@gmail.com>
2015-12-02 19:12:21 +0000

    Make everything configurable on the CLI

--- bot.py
+++ bot.py
@@ -1,17 +1,9 @@
 """IRC standup bot."""
 
+import argparse
+
 import irc.client
 import irc.strings
-
-
-USERS = [
-    'dolphm',  # Dolph Mathews
-    'lbragstad',  # Lance Bragstad
-    'dstanek',  # David Stanek
-    'rderose',  # Ron de Rose
-    'nputschi',  # Navid Putschi
-    'nonameentername',  # Werner ...
-    'jorge_munoz']  # Jorge Munoz
 
 
 class StandupBot(irc.client.SimpleIRCClient):
@@ -21,13 +13,16 @@ class StandupBot(irc.client.SimpleIRCClient):
 
     """
 
-    def __init__(self, channel, nickname, server, port=6667):
+    def __init__(self, channel, nickname, server, port=6667,
+                 users_to_ping=None, standup_duration=60 * 15):
         """Initialize the IRC client."""
         super(StandupBot, self).__init__()
+        self.channel = channel
+        self.users_to_ping = users_to_ping or []
+        self.standup_duration = standup_duration
 
         print('Connecting...')
         self.connect(server, port, nickname)
-        self.channel = channel
 
         # We start the standup as soon as we know who is in the channel. But
         # for some reason, we get two responses when we request the list of
@@ -50,8 +45,12 @@ class StandupBot(irc.client.SimpleIRCClient):
         print('Requesting names on channel...')
         connection.names(self.channel)
 
-        # Disconnect after 15 minutes.
-        connection.execute_delayed(delay=60 * 15, function=connection.quit)
+        # Disconnect later, after the standup is over.
+        print('Disconnecting after %d seconds.' % self.standup_duration)
+        connection.execute_delayed(
+            delay=self.standup_duration,
+            function=connection.quit,
+            arguments=('Standup ended!',))
 
     def on_disconnect(self, connection, event):
         """Exit cleanly."""
@@ -59,38 +58,43 @@ class StandupBot(irc.client.SimpleIRCClient):
 
     def on_namreply(self, connection, event):
         """Start the standup."""
+        # Bail if we've already started a standup.
         if self._already_started_standup:
             return
-
         self._already_started_standup = True
 
+        # The list of names is space-delimited. Break it apart so we can
+        # iterate through it.
         list_of_names = event.arguments[-1].split(' ')
 
         # Strip user modes from names.
         list_of_names = [x.lstrip('@') for x in list_of_names]
 
         # Filter list of names for ones we care about.
-        list_of_names = [x for x in list_of_names if x in USERS]
+        list_of_names = [x for x in list_of_names if x in self.users_to_ping]
 
         # Build a pretty ping message.
-        comma_separated_names = ', '.join(sorted(list_of_names))
-        message = '%s: standup time!' % comma_separated_names
+        message = ''
+        if list_of_names:
+            message = ', '.join(sorted(list_of_names))
+            message += ': '
+        message += 'Standup time!'
 
         # Send the standup message.
         connection.privmsg(self.channel, message)
         connection.privmsg(self.channel, 'What are you working on today, and '
                                          'what do you need help with?')
 
-        for user in USERS:
+        # Ping the users that are not in the channel privately.
+        for user in self.users_to_ping:
             if user not in list_of_names:
-                connection.privmsg(user, 'Standup time in #osic-keystone')
+                connection.privmsg(user, 'Standup time in %s' % self.channel)
 
     def on_pubmsg(self, connection, event):
         """Do nothing."""
         print(
             'Received pubmsg: %s (source=%s, type=%s, target=%s, '
             'arguments=%s, tags=%s)' % (
-                event,
                 event.source,
                 event.type,
                 event.target,
@@ -102,7 +106,6 @@ class StandupBot(irc.client.SimpleIRCClient):
         print(
             'Received privmsg: %s (source=%s, type=%s, target=%s, '
             'arguments=%s, tags=%s)' % (
-                event,
                 event.source,
                 event.type,
                 event.target,
@@ -111,9 +114,36 @@ class StandupBot(irc.client.SimpleIRCClient):
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'server', help='The IRC server to connect to.')
+    parser.add_argument(
+        '--port', default=6667, help='The IRC server\'s port.')
+    parser.add_argument(
+        'channel', help='The IRC channel to hold the standup in.')
+    parser.add_argument(
+        'nickname', help='The IRC nickname to use.')
+
+    parser.add_argument(
+        'users_to_ping',
+        metavar='nickname-to-ping', type=str, nargs='+',
+        help='List of users to ping at the beginning of the standup.')
+
+    parser.add_argument(
+        '--standup-duration',
+        type=int, default=60 * 15,
+        help='Standup duration in seconds (the default is 15 minutes).')
+
+    args = parser.parse_args()
+
+    if not args.channel.startswith('#'):
+        args.channel = '#%s' % args.channel
+
     bot = StandupBot(
-        channel='#osic-keystone',
-        nickname='dolphm_bot',
-        server='chat.freenode.net',
-        port=6667)
+        channel=args.channel,
+        nickname=args.nickname,
+        server=args.server,
+        port=args.port,
+        users_to_ping=args.users_to_ping,
+        standup_duration=args.standup_duration)
     bot.start()
